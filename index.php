b858ec2a3657813860dc09e05227100eccba4124
Miki Nagai <miki.n@knights.ucf.edu>
2013-11-11 21:10:46 -0500

    Updated - navigation fixed

--- index.php
+++ index.php
@@ -1,35 +1,54 @@
-<?php
-include 'header.php';
-?>
-			<div class="main">
-                <img src="img/logo-main.png" alt="logo"/>
-                <div style="text-align:center;">
-                	<p id="description">
+<!doctype html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+<meta charset="UTF-8">
+<title>Moodster</title>
+
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script src="js/custom.js"></script>
+
+</head>
+
+<body>
+
+<div class="container">
+  
+	<div class="menu_container">
+		<a id="menu-toggle" class="anchor-link" href="#mobile-nav"><span id="n">&#9776;</span></a>
+	</div>
+	
+	<nav id="#mobile-nav" class="nav clearfix" role="navigation">
+		<ul>
+			<li><a id="mainlogo" class="logo" href="index.php"><img src="img/moodster.gif" alt="logo" /></a></li>
+			<li><a href="music.php"><span><img src="img/icon-music.png" alt="music" /></span>music</a></li>
+			<li><a href="video.php"><span><img src="img/icon-video.png" alt="video" /></span>video</a></li>
+			<li><a href="image.php"><span><img src="img/icon-image.png" alt="image" /></span>images</a></li>
+		</ul>
+	</nav>
+	
+
+	<section class="main_content" role="main">
+	
+        <img src="img/logo-main.png" id="logo" alt="logo" />
+        <p id="description">
                     	Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget.
                     </p>
-                        <div id="dd" class="wrapper-dropdown">Select Mood
-                            <ul class="dropdown">
-                                <li><a href="#">Happy</a></li>
-                                <li><a href="#">Sad</a></li>
-                                <li><a href="#">Angry</a></li>
-                            </ul>
-                        </div> <!--dd-->
-                        
-                        <p id="quotes"><?php include 'quotes.php'; ?></p>
-                        
-                        <!--
-                        <select id="mood" style="margin-bottom:10px;">
-                            <option>Happy</option>
-                            <option>Sad</option>
-                            <option>Excited</option>
-                            <option>Angry</option>
-                            <option>Relaxed</option>
-                        </select>
-                        -->
-				<div class="player"></div> 
-                </div><!--section-->
-			</div><!--main-->
+				
 		
-<?php
-include 'footer.php';
-?>
+		
+	</section>
+			
+			
+	<footer class="clearfix footer">
+			
+		<p id="quotes"><?php include 'quotes.php'; ?></p>
+				
+	</footer>
+
+
+</div><!-- END .container -->
+        
+</body>
+</html>
--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,29 @@
-var menuLeft = document.getElementById( 'main-menu' )
-				showLeftPush = document.getElementById( 'showLeftPush' ),
-				body = document.body;
+$(document).ready(function() {
+  
+  	function checkMenu(){
+  		if($('body').hasClass('active')){
+    	alert('moved');
+    	}
+    	else{
+    		$('nav[role=navigation]').css('width', '20%');
+    		$('nav[role=navigation]').css('float', 'left');
+    		$('section.main_content').css('float', 'left');
+    		$('section.main_content').css('width', '80%');
+    		$('.container').css('margin', '0');
+    	}
+  	}
 
-			showLeftPush.onclick = function() {
-				classie.toggle( this, 'active' );
-				classie.toggle( body, 'cbp-spmenu-push-toright' );
-				classie.toggle( menuLeft, 'cbp-spmenu-open' );
-				disableOther( 'showLeftPush' );
-			};
+  	checkMenu();
 
-			function disableOther( button ) {
-				if( button !== 'showLeftPush' ) {
-					classie.toggle( showLeftPush, 'disabled' );
-				}
-				
-			}
+	window.scrollTo(0, 1);
+	
+	$('#menu-toggle').click(function (e) {
+		$('body').toggleClass('active');
+		e.preventDefault();
+		
+    });
 
+
+    
+    
+});
